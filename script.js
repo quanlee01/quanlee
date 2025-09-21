3d5b6e969f81dc7866a374b97de346090eb8b6fb
quanlee01 <quanmcvn91@gmail.com>
2025-09-21 22:52:38 +0700

    made stuff

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-function eat() 
+function eat()
 {
   alert("nhớ ăn cơm nghaennn");
+  an.style.display = "none";
   return;
 }
+const an = document.getElementById("an");
+an.addEventListener("click",eat);
+let noCnt = 1;
+let shown = Boolean(0);
+function yes() 
+{
+  an.style.display = "block";
+  shown = 1;
+  return;
+}
+let scale = 1;
+function no()
+{
+  if(noCnt < 3) 
+  {
+    co.style.transform = `scale(${scale*=1.5})`;
+    noCnt++;
+    return;
+  }
+  else if(!shown)
+  {
+    an.style.display = "block";
+    noCnt = 0;
+  }
+  return;
+}
+
+const khong = document.getElementById("no");
+khong.addEventListener("click",no);
+const co = document.getElementById("yes");
+co.addEventListener("click",yes);
